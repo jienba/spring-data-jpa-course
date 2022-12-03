f5d5fab242631cf4028073ede205f036397e8fa5
Adama Dieng BA <aba@modelsis.sn>
2022-12-03 15:31:06 +0000

    Setting up section 1

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,60 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student jienba = new Student(
+                    "Adama D.",
+                    "BA",
+                    "adamajb@techsuki.sn",25);
+            Student jiraya = new Student(
+                    "Almamy D.",
+                    "Camara",
+                    "jiraya@techsuki.sn",24);
+
+            System.out.println("Adding jienba and jiraya");
+            studentRepository.saveAll(List.of(jienba, jiraya));
+
+            System.out.print("Number of students:");
+            System.out.println(studentRepository.count());
+
+            studentRepository
+                    .findById(1L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 1 not found!")
+            );
+            studentRepository
+                    .findById(3L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 3 not found!")
+            );
+
+            System.out.println("Select all students");
+            List<Student> students = studentRepository.findAll();
+            students.forEach(System.out::println);
+
+            System.out.println("Delete jiraya");
+            studentRepository.deleteById(2L);
+
+            System.out.print("Number of students:");
+            System.out.println(studentRepository.count());
+        };
     }
 
 }
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -14,55 +14,24 @@ import static javax.persistence.GenerationType.SEQUENCE;
 public class Student {
 
     @Id
-    @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = SEQUENCE,
-            generator = "student_sequence"
-    )
-    @Column(
-            name = "id",
-            updatable = false
-    )
+    @SequenceGenerator(name = "student_sequence",sequenceName = "student_sequence",allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE,generator = "student_sequence")
+    @Column(name = "id",updatable = false)
     private Long id;
 
-    @Column(
-            name = "first_name",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+    @Column(name = "first_name",nullable = false,columnDefinition = "TEXT")
     private String firstName;
 
-    @Column(
-            name = "last_name",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+    @Column(name = "last_name",nullable = false,columnDefinition = "TEXT")
     private String lastName;
 
-    @Column(
-            name = "email",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+    @Column(name = "email",nullable = false,columnDefinition = "TEXT")
     private String email;
 
-    @Column(
-            name = "age",
-            nullable = false
-
-    )
+    @Column(name = "age", nullable = false)
     private Integer age;
 
-    public Student(Long id,
-                   String firstName,
-                   String lastName,
-                   String email,
-                   Integer age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
