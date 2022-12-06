fda3160cf0fc419d19f309842bf95784435b3ffd
Adama Dieng BA <aba@modelsis.sn>
2022-12-06 22:41:59 +0000

    Section 3 done

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -17,35 +19,22 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
-            Student jienba = new Student(
-                    "Adama",
-                    "BA",
-                    "adamajb@techsuki.sn",25);
-            Student jienba2 = new Student(
-                    "Adama",
-                    "BA",
-                    "adamajb2@techsuki.sn",45);
-            Student jiraya = new Student(
-                    "Almamy D.",
-                    "Camara",
-                    "jiraya@techsuki.sn",24);
-
-            studentRepository.saveAll(List.of(jienba, jiraya, jienba2));
-
-            studentRepository
-                    .findStudentsByFirstNameEqualsAndAgeGreaterThanEqual("Adama", 25)
-                    .forEach(System.out::println);
-
-            /*studentRepository
-                    .findStudentsByFirstNameEqualsAndAgeGreaterThanEqualNative("Adama", 25)
-                    .forEach(System.out::println);*/
-
-            System.out.println("Deleting Adama 3");
-            System.out.println(studentRepository.deleteStudentById(3L));
-
-
-
+            generateRandomStudent(studentRepository);
+            Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
+            studentRepository.findAll(sort)
+                    .forEach(() -> System.out::println);
         };
+    }
+
+    private void generateRandomStudent(StudentRepository studentRepository) {
+        var faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@techsuki.sn",firstName,lastName);
+            Student student = new Student(firstName, lastName, email, faker.number().numberBetween(17, 45));
+            studentRepository.save(student);
+        }
     }
 
 }
