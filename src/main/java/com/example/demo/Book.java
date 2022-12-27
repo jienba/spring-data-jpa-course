3acc8ccc9d2ada5813f87dbfbe75a5b9ed162643
Adama Dieng BA <aba@modelsis.sn>
2022-12-27 15:17:48 +0000

    1- Exercise Unidirectional ManyToOne

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Book")
+@Table(name = "book")
+public class Book {
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence_generator",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "book_sequence_generator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    @Column(name = "book_name", nullable = false, columnDefinition = "TEXT")
+    private String bookName;
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "student_id",
+            referencedColumnName = "id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "student_id_foreignKey_book")
+    )
+    private Student student;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookName='" + bookName + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Book() {
+    }
+
+    public Book(String bookName, LocalDateTime createdAt, Student student) {
+        this.bookName = bookName;
+        this.createdAt = createdAt;
+        this.student = student;
+    }
+}
