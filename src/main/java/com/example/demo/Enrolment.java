3002a283f2b6736d0900af71cfc9347f2750af0e
Adama Dieng BA <aba@modelsis.sn>
2023-01-25 22:37:22 +0000

    7-@EmbeddedId and @MapsId

---
+++ src/main/java/com/example/demo/Enrolment.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity(name = "Enrolment")
+@Table(name = "enrolment")
+public class Enrolment {
+
+    @EmbeddedId
+    private  EnrolmentId id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    @MapsId("studentId") // Used for specifying part key for composeite key
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    @MapsId("courseId")
+    private Course course;
+
+    public Enrolment() {
+    }
+
+    public Enrolment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public EnrolmentId getId() {
+        return id;
+    }
+
+    public void setId(EnrolmentId id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
